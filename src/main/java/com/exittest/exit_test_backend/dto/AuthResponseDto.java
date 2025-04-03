dc65236e6059fd2f67266af12d98f914b40d607c
Tshegofatso Sekgothe <tshegofatso.sekgothe@nagarro.com>
2025-04-03 11:52:18 +0200

    custom data transfer objects done

---
+++ src/main/java/com/exittest/exit_test_backend/dto/AuthResponseDto.java
@@ -0,0 +1,10 @@
+package com.exittest.exit_test_backend.dto;
+
+public class AuthResponseDto {
+
+    private final String token;
+
+    public AuthResponseDto(String token){
+        this.token = token;
+    }
+}
