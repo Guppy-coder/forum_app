40c1fdc097338b4a556eaf232c438c8f260785e5
Tshegofatso Sekgothe <tshegofatso.sekgothe@nagarro.com>
2025-04-03 11:51:09 +0200

    custom authentication service done

---
+++ src/main/java/com/exittest/exit_test_backend/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.exittest.exit_test_backend.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    // custom security filter chain
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .csrf(customizer -> customizer.disable()) //disabled CSRF for requests
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults()) // for browser login
+                .httpBasic(Customizer.withDefaults()) // for any rest client login
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // make request stateless
+                // server does not keep track of cookies
+                .build();
+
+        }
+
+    // used to authenticate user that is in database
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setUserDetailsService(userDetailsService); // setting details service that will get any user in database
+        return provider;
+    }
+}
