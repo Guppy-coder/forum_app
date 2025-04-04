d659234d4d373bbf7e420ed1095ad912f6bbb032
Tshegofatso Sekgothe <tshegofatso.sekgothe@nagarro.com>
2025-04-04 08:34:26 +0200

    verification and jwttoken generation added

--- src/main/java/com/exittest/exit_test_backend/service/UserService.java
+++ src/main/java/com/exittest/exit_test_backend/service/UserService.java
@@ -1,12 +1,16 @@
 package com.exittest.exit_test_backend.service;
 
+import com.exittest.exit_test_backend.dto.LoginRequestDto;
 import com.exittest.exit_test_backend.dto.RegistrationDto;
 import com.exittest.exit_test_backend.enums.Roles;
 import com.exittest.exit_test_backend.model.User;
 import com.exittest.exit_test_backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.security.core.Authentication;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +20,12 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private AuthenticationManager authManager;
+
+    @Autowired
+    private JwtService jwtService;
 
     private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
@@ -43,6 +53,18 @@ public class UserService {
 
     public List<User> getUsers(){
         return (List<User>) userRepository.findAll();
+    }
+
+    public User findByUsername(String username){
+        return userRepository.findByUsername(username);
+    }
+
+    public String verify(LoginRequestDto loginRequest) {
+        Authentication authentication = authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword())
+        );
+
+        return jwtService.generateToken(loginRequest.getUsername());
     }
 }
 
