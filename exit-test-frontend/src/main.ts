a466215166485dbe6ec8a726118428f56093db61
Tshegofatso Sekgothe <tshegofatso.sekgothe@nagarro.com>
2025-04-10 11:24:30 +0200

    added routes to components

--- exit-test-frontend/src/main.ts
+++ exit-test-frontend/src/main.ts
@@ -1,6 +1,25 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { LoginComponent } from './app/components/login/login.component';
+import { LandingpageComponent } from './app/components/landingpage/landingpage.component';
+import { RegistrationComponent } from './app/components/registration/registration.component';
+import { HomeComponent } from './app/components/homepage/homepage.component';
+import { PostComponent } from './app/components/post/post.component';
+import { CommentPageComponent } from './app/components/comment-page/comment-page.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    provideRouter([
+      { path: '', component: LandingpageComponent },
+      { path: 'login', component: LoginComponent },
+      {path: 'signup', component: RegistrationComponent},
+      {path: 'dashboard', component: HomeComponent},
+      {path: 'post', component: PostComponent},
+      {path: 'questions/:id', component: CommentPageComponent},
+
+    ])
+  ]
+});
